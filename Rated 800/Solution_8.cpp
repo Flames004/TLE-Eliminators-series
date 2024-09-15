6d015ced4fc9e284f3702a00691745d3bf6ba155
Deepak Shukla <flamesofwisdom004@gmail.com>
2024-09-15 13:11:21 +0530

    Updated the solution

--- Rated 800/Solution_8.cpp
+++ Rated 800/Solution_8.cpp
@@ -1,5 +1,7 @@
 /*
 Approach :
+If k is present in the array then there always exist a susegment of k appearing most number of times.
+So print yes if k exists in the array otherwise print no.
 
 */
 
@@ -8,6 +10,21 @@ using namespace std;
 
 int main()
 {
-    
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++)
+            cin >> arr[i];
+
+        if (find(arr.begin(), arr.end(), k) != arr.end())
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
     return 0;
 }
