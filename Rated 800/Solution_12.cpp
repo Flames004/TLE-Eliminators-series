e2beea1b59c10a70a88ce01a58450f74701c1e0e
Deepak Shukla <flamesofwisdom004@gmail.com>
2024-09-18 23:49:56 +0530

    Updated the solution

--- Rated 800/Solution_12.cpp
+++ Rated 800/Solution_12.cpp
@@ -9,11 +9,35 @@ using namespace std;
 int main()
 {
     int t;
-    cin>>t;
-    while(t--){
+    cin >> t;
+    while (t--)
+    {
         int n;
-        cin>>n;
-        
+        cin >> n;
+
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++)
+            cin >> arr[i];
+
+        vector<int> ans;
+        ans.push_back(arr[0]);
+        for (int i = 1; i < n; i++)
+        {
+            if (arr[i] >= arr[i - 1])
+            {
+                ans.push_back(arr[i]);
+            }
+            else
+            {
+                ans.push_back(arr[i]);
+                ans.push_back(arr[i]);
+            }
+        }
+
+        cout << ans.size() << endl;
+        for (auto x : ans)
+            cout << x << " ";
+        cout << endl;
     }
     return 0;
 }
